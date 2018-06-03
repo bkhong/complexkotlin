466d4e055ca34d4eaa7654e1c0b1622dd04ce4b9
Brandon Hong <brandonhong@D-10-19-16-31.dhcp4.washington.edu>
2018-06-02 21:04:23 -0700

    completed assignment

--- main.kts
+++ main.kts
@@ -5,7 +5,18 @@ println("UW Complex Kotlin homework")
 // use fold to compress the array of strings down into a single string
 // the final string should look like FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ
 //
-val mapFoldResults = ""
+val mapFoldResults = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15)
+        .map({
+            i: Int ->
+            var result: String = ""
+            if (i % 3 == 0) {
+                result += "FIZZ"
+            }
+            if (i % 5 == 0) {
+                result += "BUZZ"
+            }
+            result
+        }).fold("", { prev: String, curr: String -> prev + curr })
 
 
 // This is a utility function for your use as you choose, and as an
@@ -20,25 +31,53 @@ fun Int.times(block: () -> Unit): Unit {
 fun process(message: String, block: (String) -> String): String {
     return ">>> ${message}: {" + block(message) + "}"
 }
-val r1 = "" // call process() with message "FOO" and a block that returns "BAR"
+
+val r1 = process("FOO", {_ -> "BAR"})
 
 val r2_message = "wooga"
-val r2 = "" // call process() with message "FOO" and a block that upper-cases 
-            // r2_message, and repeats it three times with no spaces: "WOOGAWOOGAWOOGA"
+val r2 = process("FOO", {
+    _ ->
+    var result: String = ""
+    for (i in 1..3) {
+        result += r2_message.toUpperCase()
+    }
+    result
+})
 
 
 // write an enum-based state machine between talking and thinking
-enum class Philosopher { }
+enum class Philosopher {
+    THINKING {
+        override fun signal() = TALKING
+        override fun toString(): String {
+            return "Deep thoughts...."
+        }
+    },
+    TALKING {
+        override fun signal() = THINKING
+        override fun toString(): String {
+            return "Allow me to suggest an idea..."
+        }
+    };
+
+    abstract fun signal(): Philosopher
+}
 
 // create an class "Command" that can be used as a function (provide an "invoke()" function)
 // that takes a single parameter ("message" of type String)
 // primary constructor should take a String argument ("prompt")
 // when called, the Command object should return a String containing the prompt and then the message
 class Command(val prompt: String) {
+    operator fun invoke(message: String): String {
+        return prompt + message
+    }
 }
 
 
-
+// Extra Credit
+// 1. Seneca the Younger was a Roman philosopher, statesman, dramatist, and satirist. He wrote tragic plays and essays that negotiated with morality.
+// 2. Seneca the Younger is most often associated with the School of Stoicism.
+// 3. The School of Stoicism is a pragmatic way of life with a goal of overcoming destructive emotions.
 
 // ================================
 println("map fold test: " + if (mapFoldResults == "FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ") "." else "!")
@@ -62,6 +101,3 @@ print("Command tests: ")
 print(if (Command("")("") == "") "." else "!")
 print(if (Command("> ")("Hello!") == "> Hello!") "." else "!")
 println("")
-
-
-
